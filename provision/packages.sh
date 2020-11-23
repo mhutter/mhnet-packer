b95405bf56f9d535dfc55f3e3e99a447ecd71a4b
Manuel Hutter <manuel@vshn.ch>
2020-11-24 00:08:21 +0100

    base: Remove some useless log output

--- provision/packages.sh
+++ provision/packages.sh
@@ -24,7 +24,7 @@ while fuser -s /var/{lib/{dpkg,apt/lists},cache/apt/archives}/lock; do
   sleep 2
 done
 
-apt-get update -yq
+apt-get update -qq
 apt-get purge -yq --autoremove "${packages_to_remove[@]}"
 apt-get dist-upgrade -yq
 apt-get install -yq "${packages_to_install[@]}"
